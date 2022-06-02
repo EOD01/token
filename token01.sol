bdcaf437e9d3ce50571862afbcc0a87ed39965dd
"EOD01" <"hardnut@hardnut.com">
2022-06-02 18:26:40 +0100

    add Pausable

--- token01.sol
+++ token01.sol
@@ -2,11 +2,12 @@
 
 pragma solidity ^0.8.0;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 
-contract token01 is ERC20, AccessControl {
+contract token01 is ERC20, AccessControl, Pausable {
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
@@ -15,27 +16,28 @@ contract token01 is ERC20, AccessControl {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    modifier validDestination(address to) {
-        require(!Address.isContract(to), "Cannot send to a contract!");
-        _;
-    }
-
-    function mint(address to, uint256 amount)
-        public onlyRole(MINTER_ROLE) validDestination(to)
-    {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount)
-        public onlyRole(BURNER_ROLE)
-    {
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
         _burn(from, amount);
     }
 
+    function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
+    }
+
     function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal virtual override validDestination(to)
+        internal virtual override
     {
         super._beforeTokenTransfer(from, to, amount);
+        require(!paused(), "Cannot transfer while paused");
+        require(!Address.isContract(to), "Cannot send to a contract!");
     }
 
 }
